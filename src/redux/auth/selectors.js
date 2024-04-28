85f9e37d45be266844f443d3d606408718c5be80
ukrainian-bird84 <143319836+ukrainian-bird84@users.noreply.github.com>
2024-04-28 23:53:30 +0200

    jolih

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -7,3 +7,4 @@ export const selectIsRefreshing = (state) => state.auth.isRefreshing;
 export const selectIsLoading = (state) => state.auth.isLoading;
 export const selectIsError = (state) => state.auth.isError;
 export const selectFilteredContacts = (state) => state.auth.FilteredContacts;
+export const selectNameFilter = (state) => state.auth.NameFilter;
